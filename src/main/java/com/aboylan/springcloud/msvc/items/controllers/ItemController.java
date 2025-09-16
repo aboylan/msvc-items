a25628192000ae0e2231e4ea72c0c576bf9a288d
Alex Boylan <aboylan@users.noreply.github.com>
2025-09-15 22:07:09 -0600

    Probando nuestra API Items (cliente) con Postman

--- src/main/java/com/aboylan/springcloud/msvc/items/controllers/ItemController.java
+++ src/main/java/com/aboylan/springcloud/msvc/items/controllers/ItemController.java
@@ -1,5 +1,6 @@
 package com.aboylan.springcloud.msvc.items.controllers;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,12 +27,13 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Item> detail(@PathVariable Long id) {
+    public ResponseEntity<?> detail(@PathVariable Long id) {
         Optional<Item> itemOptional = service.findById(id);
         if (itemOptional.isPresent()) {
             return ResponseEntity.ok(itemOptional.get());
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(404)
+                .body(Collections.singletonMap("message", "No existe el producto en el microservicio msvc-products"));
     };
 
 }
