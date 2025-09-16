cb81c478533806bcfa38d8e64ad849f96b64633d
Alex Boylan <aboylan@users.noreply.github.com>
2025-09-16 10:51:38 -0600

    Implementando componente service con cliente Http WebClient

--- src/main/java/com/aboylan/springcloud/msvc/items/services/ItemServiceWebClient.java
+++ src/main/java/com/aboylan/springcloud/msvc/items/services/ItemServiceWebClient.java
@@ -1,8 +1,11 @@
 package com.aboylan.springcloud.msvc.items.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -19,14 +22,26 @@ public class ItemServiceWebClient implements ItemService {
 
     @Override
     public List<Item> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return this.client.build()
+                .get()
+                .uri("http://msvc-products")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Item.class)
+                .collectList()
+                .block();
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Map<String, Long> params = new HashMap<>();
+        params.put("id", id);
+
+        return Optional.ofNullable(this.client.build().get().uri("http://msvc-products/{}", params)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .block());
     }
 
 }
